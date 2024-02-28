babdef00a4fbd1aa8c752eed7f9d1e5c516e7455
Deb <debasish.chandra@accenture.com>
2024-02-28 11:15:04 +0530

    committing the signup page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Songplay from './Songplay';
 import Songlist from './Songlist';
+import SignupPage from './signupPage';
 function App() {
   return (
     <>
+    <SignupPage></SignupPage>
     <Songplay></Songplay>
     <Songlist></Songlist>
     </>
---
+++ src/signupPage.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function SignupPage() {
+  return (
+    //create two text fields one for the userid and another for the password
+    <div>
+      <h1>Signup</h1>
+      <form>
+        <label>
+          User ID:
+          <input type="text" />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" />
+        </label>
+        <br />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+    //create a button to submit the form
+    //create a link to the login page
+    //create a link to the home page
+    //create a link to the signup page
+    //create a link to the about page
+    //create a link to the contact page
+    //create a link to the faq page
+    //create a link to the privacy policy page
+    //create a link to the terms of service page
+    //create a link to the help page
+    //create a link to the support page
+    //create a link to the feedback page
+    //create a link to the blog page
+    //create a link to the community page
+  )
+}
+
+export default SignupPage
